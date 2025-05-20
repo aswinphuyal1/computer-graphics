60946d09b3ab3ade948fceb95b51e0d255f699ea
aswinphuyal1 <phuyalaswin651@gmail.com>
2025-05-20 07:36:46 +0545

    file added

---
+++ Untitled2.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+#include<math.h>
+int i,dinc;
+int main()
+{	
+	printf("**\tCompiled By Sanjog Gautam\t**\n");
+	int x1,x2,y1,y2,dx,dy;
+	printf("Enter the starting points(x1,y1): ");
+	scanf("%d%d",&x1,&y1);
+	printf("Enter the starting points(x2,y2): ");
+	scanf("%d%d",&x2,&y2);
+
+	getch();
+	closegraph();
+}
+
+
